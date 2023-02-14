db5bc1851ec6a0c8ae06706ab529e3a7e3555765
romainshark <rom.developpeurweb@gmail.com>
2023-02-14 09:58:19 +0100

    first commit

---
+++ Main.js
@@ -0,0 +1,24 @@
+console.log("aller ogcn");
+const prenom="romain";
+const nom="cilia";
+const age = 37;
+const phrase= "bonjour je m'appelle " + prenom + " " + nom + "  et j'ai "  + age + "ans";
+
+alert(phrase);
+const date=Date.now()
+const Date=new Date;
+console.log(typeof Date);
+const montableau=['toto','tati','tutu'];
+console.log(typeof montableau);
+
+varTable=new Array();
+varTable['toto']=1
+varTable['tati']=2;
+varTable['tutu']=3;
+
+const monTableau=[];
+monTableau['enfant']=1;
+monTableau['maman']=2;
+monTableau['papa']=3;
+
+
